d2f66811875e6231745d7ce5079ff8b10285a9c1
Eugene Duvenage <eugened@sahomeloans.com>
2016-10-30 13:45:49 +0200

    MINUSDM

--- src/indicators/momentumindicators/minus_dm.ts
+++ src/indicators/momentumindicators/minus_dm.ts
@@ -1,19 +1,85 @@
 import * as indicators from "../";
 import * as marketData from "../../data/market/";
-import { AbstractIndicator } from "../abstractIndicator";
 
 export class MINUSDM
-    extends AbstractIndicator<marketData.IPriceBar, number>
+    extends indicators.AbstractIndicator<marketData.IPriceBar, number>
     implements indicators.IIndicator<marketData.IPriceBar, number> {
 
-    static MINUSDM_INDICATOR_NAME: string = "MINUSDM";
-    static MINUSDM_INDICATOR_DESCR: string = "Minus Directional Movement";
+    static INDICATOR_NAME: string = "MINUSDM";
+    static INDICATOR_DESCR: string = "Minus Directional Movement";
+    static TIMEPERIOD_DEFAULT: number = 14;
+    static TIMEPERIOD_MIN: number = 1;
 
-    constructor() {
-        super(MINUSDM.MINUSDM_INDICATOR_NAME, MINUSDM.MINUSDM_INDICATOR_DESCR);
+    public timePeriod: number;
+
+    private periodCounter: number;
+    private previousHigh: number;
+    private previousLow: number;
+    private previousMinusDM: number;
+
+    constructor(timePeriod: number = MINUSDM.TIMEPERIOD_DEFAULT) {
+        super(MINUSDM.INDICATOR_NAME, MINUSDM.INDICATOR_DESCR);
+
+        if (timePeriod < MINUSDM.TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, MINUSDM.TIMEPERIOD_MIN, timePeriod)));
+        }
+
+        this.periodCounter = -1;
+
+        this.previousHigh = 0;
+        this.previousLow = 0;
+        this.previousMinusDM = 0;
+        this.timePeriod = timePeriod;
+        this.setLookBack(timePeriod - 1);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
+        this.periodCounter += 1;
+        let high = inputData.high;
+        let low = inputData.low;
+        let diffP = high - this.previousHigh;
+        let diffM = this.previousLow - low;
+
+        if (this.lookback === 1) {
+            if (this.periodCounter > 0) {
+                let result = 0;
+                if ((diffM > 0) && (diffP < diffM)) {
+                    result = diffM;
+                } else {
+                    result = 0;
+                }
+
+                this.setCurrentValue(result);
+            }
+        } else {
+            if (this.periodCounter > 0) {
+                if (this.periodCounter < this.timePeriod) {
+                    if ((diffM > 0) && (diffP < diffM)) {
+                        this.previousMinusDM += diffM;
+                    }
+
+                    if (this.periodCounter === this.timePeriod - 1) {
+                        let result = this.previousMinusDM;
+
+                        this.setCurrentValue(result);
+                    }
+                } else {
+                    let result = 0;
+                    if ((diffM > 0) && (diffP < diffM)) {
+                        result = this.previousMinusDM - (this.previousMinusDM / this.timePeriod) + diffM;
+                    } else {
+                        result = this.previousMinusDM - (this.previousMinusDM / this.timePeriod);
+                    }
+
+                    this.setCurrentValue(result);
+
+                    this.previousMinusDM = result;
+                }
+            }
+        }
+
+        this.previousHigh = high;
+        this.previousLow = low;
         return this.isReady;
     }
 }
