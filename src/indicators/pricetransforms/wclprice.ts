1a8a48b73d9f837eca887edf67b060506ac7cd6f
Eugene Duvenage <eugened@sahomeloans.com>
2016-10-30 21:14:57 +0200

    WCL

--- src/indicators/pricetransforms/wclprice.ts
+++ src/indicators/pricetransforms/wclprice.ts
@@ -1,19 +1,19 @@
 import * as indicators from "../";
 import * as marketData from "../../data/market/";
-import { AbstractIndicator } from "../abstractIndicator";
 
 export class WCLPRICE
-    extends AbstractIndicator<marketData.IPriceBar, number>
+    extends indicators.AbstractIndicator<marketData.IPriceBar, number>
     implements indicators.IIndicator<marketData.IPriceBar, number> {
 
-    static WCLPRICE_INDICATOR_NAME: string = "WCLPRICE";
-    static WCLPRICE_INDICATOR_DESCR: string = "Weighted Close Price";
+    static INDICATOR_NAME: string = "WCLPRICE";
+    static INDICATOR_DESCR: string = "Weighted Close Price";
 
     constructor() {
-        super(WCLPRICE.WCLPRICE_INDICATOR_NAME, WCLPRICE.WCLPRICE_INDICATOR_DESCR);
+        super(WCLPRICE.INDICATOR_NAME, WCLPRICE.INDICATOR_DESCR);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
+        this.setCurrentValue((inputData.high + inputData.low + (inputData.close * 2.0)) / 4.0);
         return this.isReady;
     }
 }
