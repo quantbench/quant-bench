6f1a58a36a16276b3cf1f9a09842094f540b175f
Eugene Duvenage <eugened@sahomeloans.com>
2016-10-30 09:27:14 +0200

    ADXR

--- src/indicators/momentumindicators/adxr.ts
+++ src/indicators/momentumindicators/adxr.ts
@@ -1,19 +1,53 @@
 import * as indicators from "../";
 import * as marketData from "../../data/market/";
-import { AbstractIndicator } from "../abstractIndicator";
 
 export class ADXR
-    extends AbstractIndicator<marketData.IPriceBar, number>
+    extends indicators.AbstractIndicator<marketData.IPriceBar, number>
     implements indicators.IIndicator<marketData.IPriceBar, number> {
 
-    static ADXR_INDICATOR_NAME: string = "ADXR";
-    static ADXR_INDICATOR_DESCR: string = "Average Directional Movement Index Rating";
+    static INDICATOR_NAME: string = "ADXR";
+    static INDICATOR_DESCR: string = "Average Directional Movement Index Rating";
+    static TIMEPERIOD_DEFAULT: number = 14;
+    static TIMEPERIOD_MIN: number = 2;
 
-    constructor() {
-        super(ADXR.ADXR_INDICATOR_NAME, ADXR.ADXR_INDICATOR_DESCR);
+    public timePeriod: number;
+
+    private periodCounter: number;
+    private adx: indicators.ADX;
+    private periodHistory: indicators.Queue<number>;
+
+    constructor(timePeriod: number = ADXR.TIMEPERIOD_DEFAULT) {
+        super(ADXR.INDICATOR_NAME, ADXR.INDICATOR_DESCR);
+
+        if (timePeriod < ADXR.TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, ADXR.TIMEPERIOD_MIN, timePeriod)));
+        }
+
+        this.timePeriod = timePeriod;
+        this.periodCounter = 0;
+        this.adx = new indicators.ADX(timePeriod);
+        this.adx.on("data", (data: number) => this.receiveADXData(data));
+        this.setLookBack(this.timePeriod - 1 + this.adx.lookback);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
+        this.periodCounter++;
+        this.adx.receiveData(inputData);
         return this.isReady;
+    }
+
+    receiveADXData(data: number) {
+        this.periodHistory.enqueue(data);
+
+        if (this.periodCounter > this.lookback) {
+            let adxN = this.periodHistory.peek();
+            let result = (data + adxN) / 2;
+
+            this.setCurrentValue(result);
+        }
+
+        if (this.periodHistory.count >= this.timePeriod) {
+            this.periodHistory.dequeue();
+        }
     }
 }
