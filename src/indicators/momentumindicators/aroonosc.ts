31e1088ef9b12369dc540cdec7bb86ef900c07ef
Eugene Duvenage <eugened@sahomeloans.com>
2016-10-30 10:17:45 +0200

    AROONOSC

--- src/indicators/momentumindicators/aroonosc.ts
+++ src/indicators/momentumindicators/aroonosc.ts
@@ -6,14 +6,35 @@ export class AROONOSC
     extends AbstractIndicator<marketData.IPriceBar, number>
     implements indicators.IIndicator<marketData.IPriceBar, number> {
 
-    static AROONOSC_INDICATOR_NAME: string = "AROONOSC";
-    static AROONOSC_INDICATOR_DESCR: string = "Aroon Oscillator";
+    static INDICATOR_NAME: string = "AROONOSC";
+    static INDICATOR_DESCR: string = "Aroon Oscillator";
+    static TIMEPERIOD_DEFAULT: number = 14;
+    static TIMEPERIOD_MIN: number = 2;
 
-    constructor() {
-        super(AROONOSC.AROONOSC_INDICATOR_NAME, AROONOSC.AROONOSC_INDICATOR_DESCR);
+    public timePeriod: number;
+
+    private aroon: indicators.AROON;
+
+    constructor(timePeriod: number = AROONOSC.TIMEPERIOD_DEFAULT) {
+        super(AROONOSC.INDICATOR_NAME, AROONOSC.INDICATOR_DESCR);
+
+        if (timePeriod < AROONOSC.TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, AROONOSC.TIMEPERIOD_MIN, timePeriod)));
+        }
+
+        this.timePeriod = timePeriod;
+
+        this.aroon = new indicators.AROON(timePeriod);
+        this.aroon.on("data", (data: indicators.AROONResult) => this.receiveAROONData(data));
+        this.setLookBack(timePeriod);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
+        this.aroon.receiveData(inputData);
         return this.isReady;
+    }
+
+    receiveAROONData(data: indicators.AROONResult) {
+        this.setCurrentValue(data.aroonUp - data.aroonDown);
     }
 }
