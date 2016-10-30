ab958ccf9884c291353dc64494d2785fa3976ffd
Eugene Duvenage <eugened@sahomeloans.com>
2016-10-30 08:54:46 +0200

    DX

--- src/indicators/momentumindicators/dx.ts
+++ src/indicators/momentumindicators/dx.ts
@@ -1,19 +1,61 @@
 import * as indicators from "../";
 import * as marketData from "../../data/market/";
-import { AbstractIndicator } from "../abstractIndicator";
 
 export class DX
-    extends AbstractIndicator<marketData.IPriceBar, number>
+    extends indicators.AbstractIndicator<marketData.IPriceBar, number>
     implements indicators.IIndicator<marketData.IPriceBar, number> {
 
-    static DX_INDICATOR_NAME: string = "DX";
-    static DX_INDICATOR_DESCR: string = "Directional Movement Index";
+    static INDICATOR_NAME: string = "DX";
+    static INDICATOR_DESCR: string = "Directional Movement Index";
+    static TIMEPERIOD_DEFAULT: number = 14;
+    static TIMEPERIOD_MIN: number = 2;
 
-    constructor() {
-        super(DX.DX_INDICATOR_NAME, DX.DX_INDICATOR_DESCR);
+    public timePeriod: number;
+
+    private minusDI: indicators.MINUSDI;
+    private plusDI: indicators.PLUSDI;
+    private currentPlusDi: number;
+    private currentMinusDi: number;
+
+    constructor(timePeriod: number = DX.TIMEPERIOD_DEFAULT) {
+        super(DX.INDICATOR_NAME, DX.INDICATOR_DESCR);
+
+        if (timePeriod < DX.TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, DX.TIMEPERIOD_MIN, timePeriod)));
+        }
+
+        this.timePeriod = timePeriod;
+        this.minusDI = new indicators.MINUSDI(timePeriod);
+        this.minusDI.on("data", (data: number) => this.receiveMinusDIData(data));
+        this.currentPlusDi = 0;
+        this.plusDI = new indicators.PLUSDI(timePeriod);
+        this.plusDI.on("data", (data: number) => this.receivePlusDIData(data));
+        this.currentPlusDi = 0;
+
+        this.setLookBack(timePeriod);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
+        this.minusDI.receiveData(inputData);
+        this.plusDI.receiveData(inputData);
         return this.isReady;
+    }
+
+    private receiveMinusDIData(data: number) {
+        this.currentMinusDi = data;
+    }
+
+    private receivePlusDIData(data: number) {
+        this.currentPlusDi = data;
+
+        let result = 0;
+        let tmp = this.currentMinusDi + this.currentPlusDi;
+        if (tmp !== 0) {
+            result = 100 * (Math.abs(this.currentMinusDi - this.currentPlusDi) / tmp);
+        } else {
+            result = 0;
+        }
+
+        this.setCurrentValue(result);
     }
 }
---
+++ test/indicators/momentumindicators/dx.spec.ts
@@ -0,0 +1,171 @@
+import * as indicators from "../../../src/indicators/";
+import * as chai from "chai";
+import * as path from "path";
+let jsonfile = require("jsonfile");
+
+chai.should();
+
+describe("DX Indicator", () => {
+    let sourceFile: string;
+    let taResultFile: string;
+    let sourceData: any;
+    let taResultData: any;
+    let indicator: indicators.DX;
+    let indicatorResults: number[];
+    let indicatorOnDataRasied: boolean = false;
+    let timePeriod = 14;
+
+    beforeEach(() => {
+        sourceFile = path.resolve("./test/sourcedata/sourcedata.json");
+        taResultFile = path.resolve("./test/talib-results/dx.json");
+        sourceData = jsonfile.readFileSync(sourceFile);
+        taResultData = jsonfile.readFileSync(taResultFile);
+        indicatorResults = new Array<number>(sourceData.close.length - taResultData.begIndex);
+    });
+
+    describe("when constructing", () => {
+        beforeEach(() => {
+            indicator = new indicators.DX(timePeriod);
+        });
+
+        it("should set the indicator name", () => {
+            indicator.name.should.equal(indicators.DX.INDICATOR_NAME);
+        });
+
+        it("should set the indicator description", () => {
+            indicator.description.should.equal(indicators.DX.INDICATOR_DESCR);
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when constructing with explicit non default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.DX(timePeriod + 1);
+        });
+
+        it("should set the timePeriod", () => {
+            indicator.timePeriod.should.equal(timePeriod + 1);
+        });
+    });
+
+    describe("when constructing with default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.DX();
+        });
+
+        it("should set the timePeriod", () => {
+            indicator.timePeriod.should.equal(indicators.DX.TIMEPERIOD_DEFAULT);
+        });
+    });
+
+    describe("when constructing with timePeriod less than the minimum", () => {
+        let exception: Error;
+
+        beforeEach(() => {
+            try {
+                indicator = new indicators.DX(0);
+            } catch (error) {
+                exception = error;
+            }
+        });
+
+        it("should return a correctly formatted error", () => {
+            let message = indicators.generateMinTimePeriodError(indicator.name, indicators.DX.TIMEPERIOD_MIN, 0);
+            exception.message.should.equal(message);
+        });
+    });
+
+    describe("when receiving all tick data", () => {
+        beforeEach(() => {
+            indicator = new indicators.DX(timePeriod);
+            let idx = 0;
+            sourceData.close.forEach((value: number, index: number) => {
+                if (indicator.receiveData({
+                    "high": sourceData.high[index],
+                    "low": sourceData.low[index],
+                    "open": sourceData.open[index],
+                    "close": sourceData.close[index],
+                })) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            });
+        });
+
+        it("should match the talib results", () => {
+            for (let i = 0; i < taResultData.result.outReal.length; i++) {
+                isNaN(indicatorResults[i]).should.be.false;
+                taResultData.result.outReal[i].should.be.closeTo(indicatorResults[i], 0.001);
+            }
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when receiving less tick data than the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.DX(timePeriod);
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index < indicator.lookback; index++) {
+                if (indicator.receiveData({
+                    "high": sourceData.high[index],
+                    "low": sourceData.low[index],
+                    "open": sourceData.open[index],
+                    "close": sourceData.close[index],
+                })) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should not indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(false);
+        });
+
+        it("should not have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(false);
+        });
+    });
+
+    describe("when receiving tick data equal to the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.DX(timePeriod);
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index <= indicator.lookback; index++) {
+                if (indicator.receiveData({
+                    "high": sourceData.high[index],
+                    "low": sourceData.low[index],
+                    "open": sourceData.open[index],
+                    "close": sourceData.close[index],
+                })) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should not indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(true);
+        });
+
+        it("should not have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(true);
+        });
+    });
+});
