61bf7833e43e4e31b5c057ceeaa9cd3b75a70f53
Eugene Duvenage <eugened@sahomeloans.com>
2016-11-01 17:24:32 +0200

    MOM

--- src/indicators/momentumindicators/mom.ts
+++ src/indicators/momentumindicators/mom.ts
@@ -1,19 +1,49 @@
 import * as indicators from "../";
 
-import { AbstractIndicator } from "../abstractIndicator";
-
 export class MOM
-    extends AbstractIndicator<number, number>
+    extends indicators.AbstractIndicator<number, number>
     implements indicators.IIndicator<number, number> {
 
-    static MOM_INDICATOR_NAME: string = "MOM";
-    static MOM_INDICATOR_DESCR: string = "Momentum";
+    static INDICATOR_NAME: string = "MOM";
+    static INDICATOR_DESCR: string = "Momentum";
+    static TIMEPERIOD_DEFAULT: number = 10;
+    static TIMEPERIOD_MIN: number = 2;
 
-    constructor() {
-        super(MOM.MOM_INDICATOR_NAME, MOM.MOM_INDICATOR_DESCR);
+    public timePeriod: number;
+
+    private periodHistory: indicators.Queue<number>;
+    private periodCounter: number;
+
+    constructor(timePeriod: number = MOM.TIMEPERIOD_DEFAULT) {
+        super(MOM.INDICATOR_NAME, MOM.INDICATOR_DESCR);
+
+        if (timePeriod < MOM.TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, MOM.TIMEPERIOD_MIN, timePeriod)));
+        }
+
+        this.timePeriod = timePeriod;
+        this.periodCounter = timePeriod * -1;
+        this.periodHistory = new indicators.Queue<number>();
+        this.setLookBack(timePeriod);
     }
 
     receiveData(inputData: number): boolean {
+        this.periodCounter += 1;
+        this.periodHistory.enqueue(inputData);
+
+        if (this.periodCounter > 0) {
+            // Mom = price - previousPrice
+            let previousPrice = this.periodHistory.peek();
+
+            let result = inputData - previousPrice;
+
+            this.setCurrentValue(result);
+        }
+
+        if (this.periodHistory.count > this.timePeriod) {
+            this.periodHistory.dequeue();
+        }
+
         return this.isReady;
     }
 }
