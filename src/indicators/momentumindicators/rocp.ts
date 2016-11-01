f9fa4d614d670dad4e5224de3630e2ca966db543
Eugene Duvenage <eugened@sahomeloans.com>
2016-11-01 20:46:49 +0200

    ROCP

--- src/indicators/momentumindicators/rocp.ts
+++ src/indicators/momentumindicators/rocp.ts
@@ -1,19 +1,51 @@
 import * as indicators from "../";
 
-import { AbstractIndicator } from "../abstractIndicator";
-
 export class ROCP
-    extends AbstractIndicator<number, number>
+    extends indicators.AbstractIndicator<number, number>
     implements indicators.IIndicator<number, number> {
 
-    static ROCP_INDICATOR_NAME: string = "ROCP";
-    static ROCP_INDICATOR_DESCR: string = "Rate of change Percentage: (price-prevPrice)/prevPrice";
+    static INDICATOR_NAME: string = "ROCP";
+    static INDICATOR_DESCR: string = "Rate of change Percentage: (price-prevPrice)/prevPrice";
+    static TIMEPERIOD_DEFAULT: number = 10;
+    static TIMEPERIOD_MIN: number = 2;
 
-    constructor() {
-        super(ROCP.ROCP_INDICATOR_NAME, ROCP.ROCP_INDICATOR_DESCR);
+    public timePeriod: number;
+    private periodHistory: indicators.Queue<number>;
+    private periodCounter: number;
+
+    constructor(timePeriod: number = ROCP.TIMEPERIOD_DEFAULT) {
+        super(ROCP.INDICATOR_NAME, ROCP.INDICATOR_DESCR);
+
+        if (timePeriod < ROCP.TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, ROCP.TIMEPERIOD_MIN, timePeriod)));
+        }
+
+        this.timePeriod = timePeriod;
+        this.periodCounter = timePeriod * -1;
+        this.periodHistory = new indicators.Queue<number>();
+        this.setLookBack(this.timePeriod);
     }
 
     receiveData(inputData: number): boolean {
+        this.periodCounter += 1;
+        this.periodHistory.enqueue(inputData);
+
+        if (this.periodCounter > 0) {
+            // RocP = (price - previousPrice)/previousPrice
+            let previousPrice = this.periodHistory.peek();
+
+            let result = 0;
+            if (previousPrice !== 0) {
+                result = (inputData - previousPrice) / previousPrice;
+            }
+
+            this.setCurrentValue(result);
+        }
+
+        if (this.periodHistory.count > this.timePeriod) {
+            this.periodHistory.dequeue();
+        }
+
         return this.isReady;
     }
 }
