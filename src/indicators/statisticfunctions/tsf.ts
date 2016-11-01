1c88242a48771894c254405722fa96abfe65ed1a
Eugene Duvenage <eugened@sahomeloans.com>
2016-11-01 05:47:47 +0200

    TSF

--- src/indicators/statisticfunctions/tsf.ts
+++ src/indicators/statisticfunctions/tsf.ts
@@ -1,32 +1,17 @@
-import * as indicators from "../";
+import { LINEARREGBASE } from "./linearreg_base";
 
 export class TSF
-    extends indicators.AbstractIndicator<number, number>
-    implements indicators.IIndicator<number, number> {
+    extends LINEARREGBASE {
 
     static INDICATOR_NAME: string = "TSF";
     static INDICATOR_DESCR: string = "Time Series Forecast";
     static TIMEPERIOD_DEFAULT: number = 14;
-    static TIMEPERIOD_MIN: number = 2;
-
-    public timePeriod: number;
 
     constructor(timePeriod: number = TSF.TIMEPERIOD_DEFAULT) {
-        super(TSF.INDICATOR_NAME, TSF.INDICATOR_DESCR);
-
-        if (timePeriod < TSF.TIMEPERIOD_MIN) {
-            throw (new Error(indicators.generateMinTimePeriodError(this.name, TSF.TIMEPERIOD_MIN, timePeriod)));
-        }
-
-        this.timePeriod = timePeriod;
-        // this.rollingSum = 0;
-        // this.rollingSumOfSquares = 0;
-        // this.periodCounter = 0;
-        // this.periodHistory = new Queue<number>();
-        this.setLookBack(this.timePeriod - 1);
+        super(TSF.INDICATOR_NAME, TSF.INDICATOR_DESCR, timePeriod);
     }
 
-    receiveData(inputData: number): boolean {
-        return this.isReady;
+    calculateResult(slope: number, intercept: number): number {
+        return intercept + slope * this.timePeriod;
     }
 }
