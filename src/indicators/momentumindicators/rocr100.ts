caa83d43fb92d42a7aa10d9e45c630624a068e6d
Eugene Duvenage <eugened@sahomeloans.com>
2016-11-01 20:59:51 +0200

    ROCR100

--- src/indicators/momentumindicators/rocr100.ts
+++ src/indicators/momentumindicators/rocr100.ts
@@ -1,19 +1,51 @@
 import * as indicators from "../";
 
-import { AbstractIndicator } from "../abstractIndicator";
-
 export class ROCR100
-    extends AbstractIndicator<number, number>
+    extends indicators.AbstractIndicator<number, number>
     implements indicators.IIndicator<number, number> {
 
-    static ROCR100_INDICATOR_NAME: string = "ROCR100";
-    static ROCR100_INDICATOR_DESCR: string = "Rate of change ratio 100 scale: (price/prevPrice)*100";
+    static INDICATOR_NAME: string = "ROCR100";
+    static INDICATOR_DESCR: string = "Rate of change ratio 100 scale: (price/prevPrice)*100";
+    static TIMEPERIOD_DEFAULT: number = 10;
+    static TIMEPERIOD_MIN: number = 2;
 
-    constructor() {
-        super(ROCR100.ROCR100_INDICATOR_NAME, ROCR100.ROCR100_INDICATOR_DESCR);
+    public timePeriod: number;
+    private periodHistory: indicators.Queue<number>;
+    private periodCounter: number;
+
+    constructor(timePeriod: number = ROCR100.TIMEPERIOD_DEFAULT) {
+        super(ROCR100.INDICATOR_NAME, ROCR100.INDICATOR_DESCR);
+
+        if (timePeriod < ROCR100.TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, ROCR100.TIMEPERIOD_MIN, timePeriod)));
+        }
+
+        this.timePeriod = timePeriod;
+        this.periodCounter = timePeriod * -1;
+        this.periodHistory = new indicators.Queue<number>();
+        this.setLookBack(this.timePeriod);
     }
 
     receiveData(inputData: number): boolean {
+        this.periodCounter += 1;
+        this.periodHistory.enqueue(inputData);
+
+        if (this.periodCounter > 0) {
+            // RocR100 = (price/previousPrice - 1) * 100
+            let previousPrice = this.periodHistory.peek();
+
+            let result = 0;
+            if (previousPrice !== 0) {
+                result = (inputData / previousPrice) * 100;
+            }
+
+            this.setCurrentValue(result);
+        }
+
+        if (this.periodHistory.count > this.timePeriod) {
+            this.periodHistory.dequeue();
+        }
+
         return this.isReady;
     }
 }
