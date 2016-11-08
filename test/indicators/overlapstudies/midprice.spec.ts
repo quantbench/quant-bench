c8938d74eeb2d50d6d28003d2d134240df5b2cd3
Eugene Duvenage <eugened@sahomeloans.com>
2016-11-08 21:13:04 +0200

    MIDPRICE

---
+++ test/indicators/overlapstudies/midprice.spec.ts
@@ -0,0 +1,163 @@
+import * as indicators from "../../../src/indicators/";
+import * as chai from "chai";
+import * as path from "path";
+let jsonfile = require("jsonfile");
+
+chai.should();
+
+describe("MIDPRICE Indicator", () => {
+    let sourceFile: string;
+    let taResultFile: string;
+    let sourceData: any;
+    let taResultData: any;
+    let indicator: indicators.MIDPRICE;
+    let indicatorResults: number[];
+    let indicatorOnDataRasied: boolean = false;
+    let timePeriod = 14;
+
+    beforeEach(() => {
+        sourceFile = path.resolve("./test/sourcedata/sourcedata.json");
+        taResultFile = path.resolve("./test/talib-results/midprice.json");
+        sourceData = jsonfile.readFileSync(sourceFile);
+        taResultData = jsonfile.readFileSync(taResultFile);
+
+        indicatorResults = new Array<number>(
+            sourceData.close.length - taResultData.begIndex);
+    });
+
+    describe("when constructing", () => {
+        beforeEach(() => {
+            indicator = new indicators.MIDPRICE(timePeriod);
+        });
+
+        it("should set the indicator name", () => {
+            indicator.name.should.equal(indicators.MIDPRICE.INDICATOR_NAME);
+        });
+
+        it("should set the indicator description", () => {
+            indicator.description.should.equal(indicators.MIDPRICE.INDICATOR_DESCR);
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when constructing with explicit non default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.MIDPRICE(timePeriod + 1);
+        });
+
+        it("should set the timePeriod", () => {
+            indicator.timePeriod.should.equal(timePeriod + 1);
+        });
+    });
+
+    describe("when constructing with default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.MIDPRICE();
+        });
+
+        it("should set the timePeriod", () => {
+            indicator.timePeriod.should.equal(indicators.MIDPRICE.TIMEPERIOD_DEFAULT);
+        });
+    });
+
+    describe("when constructing with timePeriod less than the minimum", () => {
+        let exception: Error;
+
+        beforeEach(() => {
+            try {
+                indicator = new indicators.MIDPRICE(1);
+            } catch (error) {
+                exception = error;
+            }
+        });
+
+        it("should return a correctly formatted error", () => {
+            let message = indicators.generateMinTimePeriodError(indicator.name, indicators.MIDPRICE.TIMEPERIOD_MIN, 1);
+            exception.message.should.equal(message);
+        });
+    });
+
+    describe("when receiving all tick data", () => {
+        beforeEach(() => {
+            indicator = new indicators.MIDPRICE(timePeriod);
+            let idx = 0;
+            sourceData.close.forEach((value: number, index: number) => {
+                if (indicator.receiveData({
+                    "high": sourceData.high[index],
+                    "low": sourceData.low[index],
+                    "open": sourceData.open[index],
+                    "close": sourceData.close[index],
+                })) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            });
+        });
+
+        it("should match the talib results", () => {
+            for (let i = 0; i < taResultData.result.outReal.length; i++) {
+                isNaN(indicatorResults[i]).should.be.false;
+                taResultData.result.outReal[i].should.be.closeTo(indicatorResults[i], 0.001);
+            }
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when receiving less tick data than the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.MIDPRICE(timePeriod);
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index < indicator.lookback; index++) {
+                if (indicator.receiveData(sourceData.close[index])) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should not indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(false);
+        });
+
+        it("should not have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(false);
+        });
+    });
+
+    describe("when receiving tick data equal to the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.MIDPRICE(timePeriod);
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index <= indicator.lookback; index++) {
+                if (indicator.receiveData(sourceData.close[index])) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(true);
+        });
+
+        it("should have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(true);
+        });
+    });
+});
