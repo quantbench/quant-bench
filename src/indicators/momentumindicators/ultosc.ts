995ed981ac992b55dd7615e4b437ff7649ec9f1f
Eugene Duvenage <eugened@sahomeloans.com>
2016-11-10 21:49:22 +0200

    ULTOSC

--- src/indicators/momentumindicators/ultosc.ts
+++ src/indicators/momentumindicators/ultosc.ts
@@ -6,12 +6,121 @@ export class ULTOSC
 
     static INDICATOR_NAME: string = "ULTOSC";
     static INDICATOR_DESCR: string = "Ultimate Oscillator";
+    static TIMEPERIOD1_DEFAULT: number = 7;
+    static TIMEPERIOD1_MIN: number = 1;
+    static TIMEPERIOD2_DEFAULT: number = 14;
+    static TIMEPERIOD2_MIN: number = 1;
+    static TIMEPERIOD3_DEFAULT: number = 28;
+    static TIMEPERIOD3_MIN: number = 1;
 
-    constructor() {
+    public timePeriod1: number;
+    public timePeriod2: number;
+    public timePeriod3: number;
+
+    private bpSMA1: indicators.SMA;
+    private trSMA1: indicators.SMA;
+    private bpSMA2: indicators.SMA;
+    private trSMA2: indicators.SMA;
+    private bpSMA3: indicators.SMA;
+    private trSMA3: indicators.SMA;
+    private trueRange: indicators.TRANGE;
+
+    private buyingPressure: number;
+    private previousClose: number;
+    private average1: number;
+    private average2: number;
+    private average3: number;
+
+    constructor(timePeriod1: number = ULTOSC.TIMEPERIOD1_DEFAULT,
+        timePeriod2: number = ULTOSC.TIMEPERIOD2_DEFAULT,
+        timePeriod3: number = ULTOSC.TIMEPERIOD3_DEFAULT
+    ) {
         super(ULTOSC.INDICATOR_NAME, ULTOSC.INDICATOR_DESCR);
+
+        if (timePeriod1 < ULTOSC.TIMEPERIOD1_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, ULTOSC.TIMEPERIOD1_MIN, timePeriod1)));
+        }
+
+        if (timePeriod2 < ULTOSC.TIMEPERIOD2_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, ULTOSC.TIMEPERIOD2_MIN, timePeriod2)));
+        }
+
+        if (timePeriod3 < ULTOSC.TIMEPERIOD3_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, ULTOSC.TIMEPERIOD3_MIN, timePeriod3)));
+        }
+
+        // sort the time periods from shortest to longest
+        let periods: number[] = new Array(3);
+        let usedFlag: number[] = new Array(3);
+        let sortedPeriods: number[] = new Array(3);
+        let longestPeriod = 0;
+        let longestIndex = 0;
+        periods[0] = timePeriod1;
+        periods[1] = timePeriod2;
+        periods[2] = timePeriod3;
+        usedFlag[0] = 0;
+        usedFlag[1] = 0;
+        usedFlag[2] = 0;
+        for (let i = 0; i < 3; ++i) {
+            longestPeriod = 0;
+            longestIndex = 0;
+            for (let j = 0; j < 3; ++j) {
+                if ((usedFlag[j] === 0) && (periods[j] > longestPeriod)) {
+                    longestPeriod = periods[j];
+                    longestIndex = j;
+                }
+            }
+            usedFlag[longestIndex] = 1;
+            sortedPeriods[i] = longestPeriod;
+        }
+
+        this.timePeriod1 = sortedPeriods[2];
+        this.timePeriod2 = sortedPeriods[1];
+        this.timePeriod3 = sortedPeriods[0];
+
+        this.buyingPressure = 0;
+        this.previousClose = 0;
+
+        this.average1 = 0;
+        this.average2 = 0;
+        this.average3 = 0;
+
+        this.bpSMA1 = new indicators.SMA(this.timePeriod1);
+        this.trSMA1 = new indicators.SMA(this.timePeriod1);
+        this.bpSMA2 = new indicators.SMA(this.timePeriod2);
+        this.trSMA2 = new indicators.SMA(this.timePeriod2);
+        this.bpSMA3 = new indicators.SMA(this.timePeriod3);
+        this.trSMA3 = new indicators.SMA(this.timePeriod3);
+        this.trueRange = new indicators.TRANGE();
+
+        let maxPeriod = Math.max(Math.max(timePeriod1, timePeriod2), timePeriod3);
+        this.setLookBack(maxPeriod);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
+        this.trueRange.receiveData(inputData);
+        if (this.trueRange.isReady) {
+            this.buyingPressure = inputData.close - Math.min(inputData.low, this.previousClose);
+
+            this.bpSMA1.receiveData(this.buyingPressure);
+            this.trSMA1.receiveData(this.trueRange.currentValue);
+
+            this.bpSMA2.receiveData(this.buyingPressure);
+            this.trSMA2.receiveData(this.trueRange.currentValue);
+
+            this.bpSMA3.receiveData(this.buyingPressure);
+            this.trSMA3.receiveData(this.trueRange.currentValue);
+
+            if (this.bpSMA3.isReady) {
+                this.average1 = this.bpSMA1.currentValue / this.trSMA1.currentValue;
+                this.average2 = this.bpSMA2.currentValue / this.trSMA2.currentValue;
+                this.average3 = this.bpSMA3.currentValue / this.trSMA3.currentValue;
+
+                this.setCurrentValue(100 * ((4 * this.average1) + (2 * this.average2) + this.average3) / 7);
+            }
+        }
+
+        this.previousClose = inputData.close;
         return this.isReady;
     }
 }
---
+++ test/indicators/momentumindicators/ultosc.spec.ts
@@ -0,0 +1,223 @@
+import * as indicators from "../../../src/indicators/";
+import * as chai from "chai";
+import * as path from "path";
+let jsonfile = require("jsonfile");
+
+chai.should();
+
+describe("ULTOSC Indicator", () => {
+    let sourceFile: string;
+    let taResultFile: string;
+    let sourceData: any;
+    let taResultData: any;
+    let indicator: indicators.ULTOSC;
+    let indicatorResults: number[];
+    let indicatorOnDataRasied: boolean = false;
+    let timePeriod1 = 7;
+    let timePeriod2 = 14;
+    let timePeriod3 = 28;
+
+    beforeEach(() => {
+        sourceFile = path.resolve("./test/sourcedata/sourcedata.json");
+        taResultFile = path.resolve("./test/talib-results/ultosc.json");
+        sourceData = jsonfile.readFileSync(sourceFile);
+        taResultData = jsonfile.readFileSync(taResultFile);
+        indicatorResults = new Array<number>(sourceData.close.length - taResultData.begIndex);
+    });
+
+    describe("when constructing", () => {
+        beforeEach(() => {
+            indicator = new indicators.ULTOSC(timePeriod1, timePeriod2, timePeriod3);
+        });
+
+        it("should set the indicator name", () => {
+            indicator.name.should.equal(indicators.ULTOSC.INDICATOR_NAME);
+        });
+
+        it("should set the indicator description", () => {
+            indicator.description.should.equal(indicators.ULTOSC.INDICATOR_DESCR);
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when constructing with explicit non default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.ULTOSC(timePeriod1 + 1, timePeriod2 + 1, timePeriod3 + 1);
+        });
+
+        it("should set the timePeriod1", () => {
+            indicator.timePeriod1.should.equal(timePeriod1 + 1);
+        });
+
+        it("should set the timePeriod2", () => {
+            indicator.timePeriod2.should.equal(timePeriod2 + 1);
+        });
+
+        it("should set the timePeriod3", () => {
+            indicator.timePeriod3.should.equal(timePeriod3 + 1);
+        });
+    });
+
+    describe("when constructing with default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.ULTOSC();
+        });
+
+        it("should set the timePeriod1", () => {
+            indicator.timePeriod1.should.equal(indicators.ULTOSC.TIMEPERIOD1_DEFAULT);
+        });
+
+        it("should set the timePeriod2", () => {
+            indicator.timePeriod2.should.equal(indicators.ULTOSC.TIMEPERIOD2_DEFAULT);
+        });
+
+        it("should set the timePeriod3", () => {
+            indicator.timePeriod3.should.equal(indicators.ULTOSC.TIMEPERIOD3_DEFAULT);
+        });
+    });
+
+    describe("when constructing with timePeriod1 less than the minimum", () => {
+        let exception: Error;
+
+        beforeEach(() => {
+            try {
+                indicator = new indicators.ULTOSC(0);
+            } catch (error) {
+                exception = error;
+            }
+        });
+
+        it("should return a correctly formatted error", () => {
+            let message = indicators.generateMinTimePeriodError(indicator.name, indicators.ULTOSC.TIMEPERIOD1_MIN, 0);
+            exception.message.should.equal(message);
+        });
+    });
+
+    describe("when constructing with timePeriod2 less than the minimum", () => {
+        let exception: Error;
+
+        beforeEach(() => {
+            try {
+                indicator = new indicators.ULTOSC(indicators.ULTOSC.TIMEPERIOD1_DEFAULT, 0);
+            } catch (error) {
+                exception = error;
+            }
+        });
+
+        it("should return a correctly formatted error", () => {
+            let message = indicators.generateMinTimePeriodError(indicator.name, indicators.ULTOSC.TIMEPERIOD2_MIN, 0);
+            exception.message.should.equal(message);
+        });
+    });
+
+    describe("when constructing with timePeriod3 less than the minimum", () => {
+        let exception: Error;
+
+        beforeEach(() => {
+            try {
+                indicator = new indicators.ULTOSC(indicators.ULTOSC.TIMEPERIOD1_DEFAULT, indicators.ULTOSC.TIMEPERIOD2_DEFAULT, 0);
+            } catch (error) {
+                exception = error;
+            }
+        });
+
+        it("should return a correctly formatted error", () => {
+            let message = indicators.generateMinTimePeriodError(indicator.name, indicators.ULTOSC.TIMEPERIOD3_MIN, 0);
+            exception.message.should.equal(message);
+        });
+    });
+
+    describe("when receiving all tick data", () => {
+        beforeEach(() => {
+            indicator = new indicators.ULTOSC(timePeriod1, timePeriod2, timePeriod3);
+            let idx = 0;
+            sourceData.close.forEach((value: number, index: number) => {
+                if (indicator.receiveData({
+                    "high": sourceData.high[index],
+                    "low": sourceData.low[index],
+                    "open": sourceData.open[index],
+                    "close": sourceData.close[index],
+                })) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            });
+        });
+
+        it("should match the talib results", () => {
+            for (let i = 0; i < taResultData.result.outReal.length; i++) {
+                isNaN(indicatorResults[i]).should.be.false;
+                taResultData.result.outReal[i].should.be.closeTo(indicatorResults[i], 0.001);
+            }
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when receiving less tick data than the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.ULTOSC(timePeriod1, timePeriod2, timePeriod3);
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index < indicator.lookback; index++) {
+                if (indicator.receiveData({
+                    "high": sourceData.high[index],
+                    "low": sourceData.low[index],
+                    "open": sourceData.open[index],
+                    "close": sourceData.close[index],
+                })) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should not indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(false);
+        });
+
+        it("should not have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(false);
+        });
+    });
+
+    describe("when receiving tick data equal to the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.ULTOSC(timePeriod1, timePeriod2, timePeriod3);
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index <= indicator.lookback; index++) {
+                if (indicator.receiveData({
+                    "high": sourceData.high[index],
+                    "low": sourceData.low[index],
+                    "open": sourceData.open[index],
+                    "close": sourceData.close[index],
+                })) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(true);
+        });
+
+        it("should have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(true);
+        });
+    });
+});
