ca729faed22aef164708f915b5087364a40dd8a7
Eugene Duvenage <eugened@sahomeloans.com>
2016-11-12 21:48:43 +0200

    PPO

--- src/indicators/momentumindicators/ppo.ts
+++ src/indicators/momentumindicators/ppo.ts
@@ -6,11 +6,59 @@ export class PPO
     static INDICATOR_NAME: string = "PPO";
     static INDICATOR_DESCR: string = "Percentage Price Oscillator";
 
-    constructor() {
+    static FAST_TIMEPERIOD_DEFAULT: number = 12;
+    static FAST_TIMEPERIOD_MIN: number = 2;
+    static SLOW_TIMEPERIOD_DEFAULT: number = 26;
+    static SLOW_TIMEPERIOD_MIN: number = 2;
+    static MATYPE_DEFAULT: indicators.MA_TYPE = indicators.MA_TYPE.SMA;
+
+    public fastTimePeriod: number;
+    public slowTimePeriod: number;
+    public maType: indicators.MA_TYPE;
+
+    private fastMA: indicators.MA;
+    private slowMA: indicators.MA;
+
+    constructor(fastTimePeriod: number = PPO.FAST_TIMEPERIOD_DEFAULT,
+        slowTimePeriod: number = PPO.SLOW_TIMEPERIOD_DEFAULT,
+        maType: indicators.MA_TYPE = PPO.MATYPE_DEFAULT) {
         super(PPO.INDICATOR_NAME, PPO.INDICATOR_DESCR);
+
+        if (fastTimePeriod < PPO.FAST_TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, PPO.FAST_TIMEPERIOD_MIN, fastTimePeriod)));
+        }
+
+        if (slowTimePeriod < PPO.SLOW_TIMEPERIOD_MIN) {
+            throw (new Error(indicators.generateMinTimePeriodError(this.name, PPO.SLOW_TIMEPERIOD_MIN, slowTimePeriod)));
+        }
+
+        this.maType = maType;
+        if (fastTimePeriod < slowTimePeriod) {
+            this.fastTimePeriod = fastTimePeriod;
+            this.slowTimePeriod = slowTimePeriod;
+        } else {
+            this.fastTimePeriod = slowTimePeriod;
+            this.slowTimePeriod = fastTimePeriod;
+        }
+
+        this.fastMA = new indicators.MA(this.fastTimePeriod, this.maType);
+        this.slowMA = new indicators.MA(this.slowTimePeriod, this.maType);
+        this.slowMA.on("data", (data: number) => this.receiveSlowMAData(data));
+
+        this.setLookBack(this.slowMA.lookback);
     }
 
     receiveData(inputData: number): boolean {
+        this.fastMA.receiveData(inputData);
+        this.slowMA.receiveData(inputData);
         return this.isReady;
+    }
+
+    private receiveSlowMAData(data: number) {
+        if (this.slowMA.currentValue === 0) {
+            this.setCurrentValue(0);
+        } else {
+            this.setCurrentValue( (this.fastMA.currentValue - this.slowMA.currentValue) / this.slowMA.currentValue * 100.0 );
+        }
     }
 }
---
+++ test/indicators/momentumindicators/ppo.spec.ts
@@ -0,0 +1,203 @@
+import * as indicators from "../../../src/indicators/";
+import * as chai from "chai";
+import * as path from "path";
+let jsonfile = require("jsonfile");
+
+chai.should();
+
+describe("PPO Indicator", () => {
+    let sourceFile: string;
+    let taResultFile: string;
+    let sourceData: any;
+    let taResultData: any;
+    let indicator: indicators.PPO;
+    let indicatorResults: number[];
+    let indicatorOnDataRasied: boolean = false;
+    let fastTimePeriod = 12;
+    let slowTimePeriod = 26;
+    let maType: indicators.MA_TYPE = indicators.MA_TYPE.SMA;
+
+    beforeEach(() => {
+        sourceFile = path.resolve("./test/sourcedata/sourcedata.json");
+        taResultFile = path.resolve("./test/talib-results/ppo.json");
+        sourceData = jsonfile.readFileSync(sourceFile);
+        taResultData = jsonfile.readFileSync(taResultFile);
+        indicatorResults = new Array<number>(sourceData.close.length - taResultData.begIndex);
+    });
+
+    describe("when constructing", () => {
+        beforeEach(() => {
+            indicator = new indicators.PPO(fastTimePeriod, slowTimePeriod, maType);
+        });
+
+        it("should set the indicator name", () => {
+            indicator.name.should.equal(indicators.PPO.INDICATOR_NAME);
+        });
+
+        it("should set the indicator description", () => {
+            indicator.description.should.equal(indicators.PPO.INDICATOR_DESCR);
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when constructing with explicit non default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.PPO(fastTimePeriod + 1, slowTimePeriod + 1, indicators.MA_TYPE.EMA);
+        });
+
+        it("should set the fastTimePeriod", () => {
+            indicator.fastTimePeriod.should.equal(fastTimePeriod + 1);
+        });
+    });
+
+    describe("when constructing with explicit non default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.PPO(fastTimePeriod + 1, slowTimePeriod + 1, indicators.MA_TYPE.EMA);
+        });
+
+        it("should set the slowTimePeriod", () => {
+            indicator.slowTimePeriod.should.equal(slowTimePeriod + 1);
+        });
+    });
+
+    describe("when constructing with explicit non default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.PPO(fastTimePeriod + 1, slowTimePeriod + 1, indicators.MA_TYPE.EMA);
+        });
+
+        it("should set the maType", () => {
+            indicator.maType.should.equal(maType + 1);
+        });
+    });
+
+    describe("when constructing with default arguments", () => {
+        beforeEach(() => {
+            indicator = new indicators.PPO();
+        });
+
+        it("should set the fastTimePeriod", () => {
+            indicator.fastTimePeriod.should.equal(indicators.PPO.FAST_TIMEPERIOD_DEFAULT);
+        });
+
+        it("should set the slowTimePeriod", () => {
+            indicator.slowTimePeriod.should.equal(indicators.PPO.SLOW_TIMEPERIOD_DEFAULT);
+        });
+
+        it("should set the maType", () => {
+            indicator.maType.should.equal(indicators.PPO.MATYPE_DEFAULT);
+        });
+    });
+
+    describe("when constructing with fastTimePeriod less than the minimum", () => {
+        let exception: Error;
+
+        beforeEach(() => {
+            try {
+                indicator = new indicators.PPO(1);
+            } catch (error) {
+                exception = error;
+            }
+        });
+
+        it("should return a correctly formatted error", () => {
+            let message = indicators.generateMinTimePeriodError(indicator.name, indicators.PPO.FAST_TIMEPERIOD_MIN, 1);
+            exception.message.should.equal(message);
+        });
+    });
+
+    describe("when constructing with slowTimePeriod less than the minimum", () => {
+        let exception: Error;
+
+        beforeEach(() => {
+            try {
+                indicator = new indicators.PPO(indicators.PPO.FAST_TIMEPERIOD_DEFAULT, 1);
+            } catch (error) {
+                exception = error;
+            }
+        });
+
+        it("should return a correctly formatted error", () => {
+            let message = indicators.generateMinTimePeriodError(indicator.name, indicators.PPO.SLOW_TIMEPERIOD_MIN, 1);
+            exception.message.should.equal(message);
+        });
+    });
+
+    describe("when receiving all tick data", () => {
+        beforeEach(() => {
+            indicator = new indicators.PPO(fastTimePeriod, slowTimePeriod, maType);
+            let idx = 0;
+            sourceData.close.forEach((value: number) => {
+                if (indicator.receiveData(value)) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            });
+        });
+
+        it("should match the talib results", () => {
+            for (let i = 0; i < taResultData.result.outReal.length; i++) {
+                isNaN(indicatorResults[i]).should.be.false;
+                taResultData.result.outReal[i].should.be.closeTo(indicatorResults[i], 0.001);
+            }
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when receiving less tick data than the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.PPO(fastTimePeriod, slowTimePeriod, maType);
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index < indicator.lookback; index++) {
+                if (indicator.receiveData(sourceData.close[index])) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should not indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(false);
+        });
+
+        it("should not have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(false);
+        });
+    });
+
+    describe("when receiving tick data equal to the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.PPO(fastTimePeriod, slowTimePeriod, maType);
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index <= indicator.lookback; index++) {
+                if (indicator.receiveData(sourceData.close[index])) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(true);
+        });
+
+        it("should have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(true);
+        });
+    });
+});
