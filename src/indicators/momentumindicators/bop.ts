fdd7edce12a5a35fa3f479215a66e96b46d919e4
Eugene Duvenage <eugened@sahomeloans.com>
2016-11-12 21:44:22 +0200

    BOP

--- src/indicators/momentumindicators/bop.ts
+++ src/indicators/momentumindicators/bop.ts
@@ -7,11 +7,21 @@ export class BOP
     static INDICATOR_NAME: string = "BOP";
     static INDICATOR_DESCR: string = "Balance Of Power";
 
+    private highLow: number;
     constructor() {
         super(BOP.INDICATOR_NAME, BOP.INDICATOR_DESCR);
+
+        this.highLow = 0;
+        this.setLookBack(0);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
+        this.highLow = inputData.high - inputData.low;
+        if (this.highLow === 0) {
+            this.setCurrentValue(0);
+        } else {
+            this.setCurrentValue((inputData.close - inputData.open) / (this.highLow));
+        }
         return this.isReady;
     }
 }
