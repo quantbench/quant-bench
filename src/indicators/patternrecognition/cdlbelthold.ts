70b309a9914538831fb19ce533b06a9be2dd0f6f
Eugene Duvenage <eugened@sahomeloans.com>
2016-12-04 11:37:21 +0200

    belthold

--- src/indicators/patternrecognition/cdlbelthold.ts
+++ src/indicators/patternrecognition/cdlbelthold.ts
@@ -1,5 +1,9 @@
 import * as indicators from "../";
 import * as marketData from "../../data/market/";
+import { SlidingWindow } from "../slidingWindow";
+import * as candleEnums from "./candleEnums";
+import { CandleSettings } from "./candleSettings";
+import { CandleStickUtils } from "./candleUtils";
 
 export class CDLBELTHOLD
     extends indicators.AbstractIndicator<marketData.IPriceBar> {
@@ -7,8 +11,26 @@ export class CDLBELTHOLD
     static INDICATOR_NAME: string = "CDLBELTHOLD";
     static INDICATOR_DESCR: string = "Belt-hold";
 
+    private bodyLongPeriodTotal: number;
+    private bodyLongAveragePeriod: number;
+    private bodyVeryShortPeriodTotal: number;
+    private bodyVeryShortAveragePeriod: number;
+    private periodCounter: number;
+    private slidingWindow: SlidingWindow<marketData.IPriceBar>;
+
     constructor() {
         super(CDLBELTHOLD.INDICATOR_NAME, CDLBELTHOLD.INDICATOR_DESCR);
+
+        this.bodyLongAveragePeriod = CandleSettings.get(candleEnums.CandleSettingType.BodyLong).averagePeriod;
+        this.bodyVeryShortAveragePeriod = CandleSettings.get(candleEnums.CandleSettingType.ShadowVeryShort).averagePeriod;
+
+        this.bodyVeryShortPeriodTotal = 0;
+        this.bodyLongPeriodTotal = 0;
+        this.periodCounter = -1;
+
+        let lookback = Math.max(this.bodyLongAveragePeriod, this.bodyVeryShortAveragePeriod);
+        this.slidingWindow = new SlidingWindow<marketData.IPriceBar>(lookback + 1);
+        this.setLookBack(lookback);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
