916a247ab92a54684eade3af4fb7bd708a20f5ed
Eugene Duvenage <eugened@sahomeloans.com>
2017-01-05 22:34:18 +0200

    CDL3INSIDE

---
+++ test/indicators/patternrecognition/cdl3inside.spec.ts
@@ -0,0 +1,125 @@
+import * as chai from "chai";
+import * as path from "path";
+import * as indicators from "../../../src/indicators/";
+let jsonfile = require("jsonfile");
+
+chai.should();
+
+describe("CDL3INSIDE Indicator", () => {
+    let sourceFile: string;
+    let taResultFile: string;
+    let sourceData: any;
+    let taResultData: any;
+    let indicator: indicators.CDL3INSIDE;
+    let indicatorResults: number[];
+    let indicatorOnDataRasied: boolean = false;
+
+    beforeEach(() => {
+        sourceFile = path.resolve("./test/sourcedata/sourcedata.json");
+        taResultFile = path.resolve("./test/talib-results/cdl3inside.json");
+        sourceData = jsonfile.readFileSync(sourceFile);
+        taResultData = jsonfile.readFileSync(taResultFile);
+
+        indicatorResults = new Array<number>(
+            sourceData.close.length - taResultData.begIndex);
+    });
+
+    describe("when constructing", () => {
+        beforeEach(() => {
+            indicator = new indicators.CDL3INSIDE();
+        });
+
+        it("should set the indicator name", () => {
+            indicator.name.should.equal(indicators.CDL3INSIDE.INDICATOR_NAME);
+        });
+
+        it("should set the indicator description", () => {
+            indicator.description.should.equal(indicators.CDL3INSIDE.INDICATOR_DESCR);
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when receiving all tick data", () => {
+        beforeEach(() => {
+            indicator = new indicators.CDL3INSIDE();
+            let idx = 0;
+            sourceData.close.forEach((value: number, index: number) => {
+                if (indicator.receiveData({
+                    "high": sourceData.high[index],
+                    "low": sourceData.low[index],
+                    "open": sourceData.open[index],
+                    "close": sourceData.close[index],
+                })) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            });
+        });
+
+        it("should match the talib results", () => {
+            for (let i = 0; i < taResultData.result.outInteger.length; i++) {
+                isNaN(indicatorResults[i]).should.be.false;
+                taResultData.result.outInteger[i].should.be.closeTo(indicatorResults[i], 0.001);
+            }
+        });
+
+        it("should match the talib lookback", () => {
+            taResultData.begIndex.should.equal(indicator.lookback);
+        });
+    });
+
+    describe("when receiving less tick data than the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.CDL3INSIDE();
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index < indicator.lookback; index++) {
+                if (indicator.receiveData(sourceData.close[index])) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should not indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(false);
+        });
+
+        it("should not have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(false);
+        });
+    });
+
+    describe("when receiving tick data equal to the lookback period", () => {
+        beforeEach(() => {
+            indicator = new indicators.CDL3INSIDE();
+            let idx = 0;
+            indicatorOnDataRasied = false;
+            indicator.on("data", () => {
+                indicatorOnDataRasied = true;
+            });
+
+            for (let index = 0; index <= indicator.lookback; index++) {
+                if (indicator.receiveData(sourceData.close[index])) {
+                    indicatorResults[idx] = indicator.currentValue;
+                    idx++;
+                }
+            }
+        });
+
+        it("the indicator should indicate that it is ready to be consumed", () => {
+            indicator.isReady.should.equal(true);
+        });
+
+        it("should have raised the ondata event", () => {
+            indicatorOnDataRasied.should.equal(true);
+        });
+    });
+});
