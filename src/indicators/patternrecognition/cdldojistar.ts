8d3070ac447b496a941048204892a75ed672afb4
Eugene Duvenage <eugened@sahomeloans.com>
2017-01-06 21:46:15 +0200

    Simplifying Logic

--- src/indicators/patternrecognition/cdldojistar.ts
+++ src/indicators/patternrecognition/cdldojistar.ts
@@ -15,6 +15,9 @@ export class CDLDOJISTAR
     private bodyDojiAveragePeriod: number;
     private bodyLongPeriodTotal: number;
     private bodyLongAveragePeriod: number;
+    private secondCandle: marketData.IPriceBar;
+    private firstCandle: marketData.IPriceBar;
+    private firstCandleColor: candleEnums.CandleColor;
     private slidingWindow: SlidingWindow<marketData.IPriceBar>;
 
     constructor() {
@@ -24,6 +27,11 @@ export class CDLDOJISTAR
         this.bodyLongAveragePeriod = CandleSettings.get(candleEnums.CandleSettingType.BodyLong).averagePeriod;
         this.bodyDojiPeriodTotal = 0;
         this.bodyLongPeriodTotal = 0;
+
+        this.secondCandle = undefined;
+        this.firstCandle = undefined;
+        this.firstCandleColor = candleEnums.CandleColor.Black;
+
         let lookback = Math.max(this.bodyDojiAveragePeriod, this.bodyLongAveragePeriod) + 1;
         this.slidingWindow = new SlidingWindow<marketData.IPriceBar>(lookback + 1);
         this.setLookBack(lookback);
@@ -34,59 +42,73 @@ export class CDLDOJISTAR
         this.slidingWindow.add(inputData);
 
         if (!this.slidingWindow.isReady) {
-            if (this.slidingWindow.samples >= this.slidingWindow.period - this.bodyLongAveragePeriod - 1 &&
-                this.slidingWindow.samples < this.slidingWindow.period - 1) {
-                let realbody = CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyLong, inputData);
-                this.bodyLongPeriodTotal += realbody;
+            if (this.isFirstCandle()) {
+                this.bodyLongPeriodTotal += CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyLong, inputData);
             }
-            if (this.slidingWindow.samples >= this.slidingWindow.period - this.bodyDojiAveragePeriod) {
-                let highlow = CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyDoji, inputData);
-                this.bodyDojiPeriodTotal += highlow;
+            if (this.isSecondCandle()) {
+                this.bodyDojiPeriodTotal += CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyDoji, inputData);
             }
             return this.isReady;
         }
 
-        let previousCandle: marketData.IPriceBar = this.slidingWindow.getItem(1);
-        let currentCandle = inputData;
+        this.firstCandle = this.slidingWindow.getItem(1);
+        this.firstCandleColor = CandleStickUtils.getCandleColor(this.firstCandle);
+        this.secondCandle = inputData;
 
-        let hasPreviousLongRealBody = CandleStickUtils.getRealBody(previousCandle) >
-            CandleStickUtils.getCandleAverage(candleEnums.CandleSettingType.BodyLong,
-                this.bodyLongPeriodTotal,
-                previousCandle);
-
-        let hasCurrentDoji = CandleStickUtils.getRealBody(currentCandle) <=
-            CandleStickUtils.getCandleAverage(candleEnums.CandleSettingType.BodyDoji,
-                this.bodyDojiPeriodTotal, currentCandle);
-
-        let hasGapUp = CandleStickUtils.getRealBodyGapUp(previousCandle, currentCandle);
-        let hasGapDown = CandleStickUtils.getRealBodyGapDown(previousCandle, currentCandle);
-
-        let previousCandleColor = CandleStickUtils.getCandleColor(previousCandle);
-
-        // 1st: long real body
-        if (hasPreviousLongRealBody &&
-            // 2nd: doji
-            hasCurrentDoji &&
-            // that gaps up if 1st is white
-            ((previousCandleColor === candleEnums.CandleColor.White && hasGapUp)
-                ||
-                // or down if 1st is black
-                (previousCandleColor === candleEnums.CandleColor.Black && hasGapDown)
-            )) {
-            this.setCurrentValue(-previousCandleColor * 100);
+        if (this.firstCandleHasLongRealBody() &&
+            this.secondCandleIsDoji() &&
+            this.secondCandleHasGap()) {
+            this.setCurrentValue(this.firstCandleColor * -100);
         } else {
             this.setCurrentValue(0);
         }
 
         this.bodyLongPeriodTotal += CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyLong,
-            previousCandle) -
+            this.firstCandle) -
             CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyLong,
                 this.slidingWindow.getItem(this.bodyLongAveragePeriod + 1));
 
-        this.bodyDojiPeriodTotal += CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyDoji, currentCandle) -
+        this.bodyDojiPeriodTotal += CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyDoji, this.secondCandle) -
             CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyDoji,
                 this.slidingWindow.getItem(this.bodyDojiAveragePeriod));
 
         return this.isReady;
+    }
+
+    private isFirstCandle(): boolean {
+        return this.slidingWindow.samples >= this.slidingWindow.period - this.bodyLongAveragePeriod - 1 &&
+            this.slidingWindow.samples < this.slidingWindow.period - 1;
+    }
+
+    private isSecondCandle(): boolean {
+        return this.slidingWindow.samples >= this.slidingWindow.period - this.bodyDojiAveragePeriod;
+    }
+
+    private firstCandleHasLongRealBody(): boolean {
+        return CandleStickUtils.getRealBody(this.firstCandle) >
+            CandleStickUtils.getCandleAverage(candleEnums.CandleSettingType.BodyLong,
+                this.bodyLongPeriodTotal,
+                this.firstCandle);
+    }
+
+    private secondCandleIsDoji(): boolean {
+        return CandleStickUtils.getRealBody(this.secondCandle) <=
+            CandleStickUtils.getCandleAverage(candleEnums.CandleSettingType.BodyDoji,
+                this.bodyDojiPeriodTotal, this.secondCandle);
+    }
+
+    private secondCandleHasGap(): boolean {
+        return (this.firstCandleColor === candleEnums.CandleColor.White && this.hasGapUp())
+            ||
+            // or down if 1st is black
+            (this.firstCandleColor === candleEnums.CandleColor.Black && this.hasGapDown());
+    }
+
+    private hasGapUp(): boolean {
+        return CandleStickUtils.getRealBodyGapUp(this.firstCandle, this.secondCandle);
+    }
+
+    private hasGapDown(): boolean {
+        return CandleStickUtils.getRealBodyGapDown(this.firstCandle, this.secondCandle);
     }
 }
