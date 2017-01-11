1e8c678839a40094b568a57cfede696b918025fe
Eugene Duvenage <eugened@sahomeloans.com>
2017-01-11 22:04:37 +0200

    TwoCrows

--- src/indicators/patternrecognition/cdl2crows.ts
+++ src/indicators/patternrecognition/cdl2crows.ts
@@ -15,10 +15,26 @@ export class CDL2CROWS
     private bodyLongAveragePeriod: number;
     private slidingWindow: SlidingWindow<marketData.IPriceBar>;
 
+    private thirdCandle: marketData.IPriceBar;
+    private secondCandle: marketData.IPriceBar;
+    private firstCandle: marketData.IPriceBar;
+    private thirdCandleColor: candleEnums.CandleColor;
+    private secondCandleColor: candleEnums.CandleColor;
+    private firstCandleColor: candleEnums.CandleColor;
+
     constructor() {
         super(CDL2CROWS.INDICATOR_NAME, CDL2CROWS.INDICATOR_DESCR);
 
         this.bodyLongAveragePeriod = CandleSettings.get(candleEnums.CandleSettingType.BodyLong).averagePeriod;
+
+        this.bodyLongPeriodTotal = 0;
+        this.thirdCandle = undefined;
+        this.secondCandle = undefined;
+        this.firstCandle = undefined;
+        this.thirdCandleColor = candleEnums.CandleColor.Black;
+        this.secondCandleColor = candleEnums.CandleColor.Black;
+        this.firstCandleColor = candleEnums.CandleColor.Black;
+
         let lookback = this.bodyLongAveragePeriod + 2;
         this.slidingWindow = new SlidingWindow<marketData.IPriceBar>(lookback + 1);
         this.setLookBack(lookback);
@@ -31,10 +47,49 @@ export class CDL2CROWS
             return this.isReady;
         }
 
+        this.firstCandle = this.slidingWindow.getItem(2);
+        this.secondCandle = this.slidingWindow.getItem(1);
+        this.thirdCandle = inputData;
+        this.thirdCandleColor = CandleStickUtils.getCandleColor(this.thirdCandle);
+        this.secondCandleColor = CandleStickUtils.getCandleColor(this.secondCandle);
+        this.firstCandleColor = CandleStickUtils.getCandleColor(this.firstCandle);
+
+        if (this.hasFirstWhiteCandleWithLongRealBody() &&
+            this.hasSecondBlackCandleThatGapsUp() &&
+            this.hasThirdBlackCandleOpeningWithSecondAndClosingWithinFirstRealBody()
+        ) {
+            this.setCurrentValue(-100);
+        } else {
+            this.setCurrentValue(0);
+        }
+
         return this.isReady;
     }
 
     private seedSlidingWindow(inputData: marketData.IPriceBar) {
+        this.bodyLongPeriodTotal += CandleStickUtils.getCandleRange(candleEnums.CandleSettingType.BodyLong, inputData);
+    }
 
+    private hasFirstWhiteCandleWithLongRealBody() {
+        return this.firstCandleColor === candleEnums.CandleColor.White &&
+            CandleStickUtils.getRealBody(this.firstCandle) >
+            CandleStickUtils.getCandleAverage(candleEnums.CandleSettingType.BodyLong,
+                this.bodyLongPeriodTotal, this.firstCandle);
+    }
+
+    private hasSecondBlackCandleThatGapsUp() {
+        return this.secondCandleColor === candleEnums.CandleColor.Black &&
+            this.hasGapUp(this.firstCandle, this.secondCandle);
+    }
+
+    private hasThirdBlackCandleOpeningWithSecondAndClosingWithinFirstRealBody() {
+        return this.thirdCandle.open < this.secondCandle.open &&
+            this.thirdCandle.open > this.secondCandle.close &&
+            this.thirdCandle.close > this.firstCandle.open &&
+            this.thirdCandle.close < this.firstCandle.close;
+    }
+
+    private hasGapUp(firstCandle: marketData.IPriceBar, secondCandle: marketData.IPriceBar): boolean {
+        return CandleStickUtils.getRealBodyGapUp(firstCandle, secondCandle);
     }
 }
