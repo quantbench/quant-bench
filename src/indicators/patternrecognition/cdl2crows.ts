7a6f839fb0c16a0132a79ef4200125ae0eddce66
Eugene Duvenage <eugened@sahomeloans.com>
2017-01-11 20:27:21 +0200

    In Progress

--- src/indicators/patternrecognition/cdl2crows.ts
+++ src/indicators/patternrecognition/cdl2crows.ts
@@ -1,5 +1,9 @@
 import * as indicators from "../";
 import * as marketData from "../../data/market/";
+import { SlidingWindow } from "../slidingWindow";
+import * as candleEnums from "./candleEnums";
+import { CandleSettings } from "./candleSettings";
+import { CandleStickUtils } from "./candleUtils";
 
 export class CDL2CROWS
     extends indicators.AbstractIndicator<marketData.IPriceBar> {
@@ -7,11 +11,30 @@ export class CDL2CROWS
     static INDICATOR_NAME: string = "CDL2CROWS";
     static INDICATOR_DESCR: string = "Two Crows";
 
+    private bodyLongPeriodTotal: number;
+    private bodyLongAveragePeriod: number;
+    private slidingWindow: SlidingWindow<marketData.IPriceBar>;
+
     constructor() {
         super(CDL2CROWS.INDICATOR_NAME, CDL2CROWS.INDICATOR_DESCR);
+
+        this.bodyLongAveragePeriod = CandleSettings.get(candleEnums.CandleSettingType.BodyLong).averagePeriod;
+        let lookback = this.bodyLongAveragePeriod + 2;
+        this.slidingWindow = new SlidingWindow<marketData.IPriceBar>(lookback + 1);
+        this.setLookBack(lookback);
     }
 
     receiveData(inputData: marketData.IPriceBar): boolean {
+
+        if (!this.slidingWindow.isReady) {
+            this.seedSlidingWindow(inputData);
+            return this.isReady;
+        }
+
         return this.isReady;
+    }
+
+    private seedSlidingWindow(inputData: marketData.IPriceBar) {
+
     }
 }
