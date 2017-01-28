5ac2aa0bfa16d73535ad95bc87cf5b9c1c185b8c
Eugene Duvenage <eugened@sahomeloans.com>
2017-01-28 21:53:50 +0200

    CDLHANGINGMAN & CDLHAMMER

--- test/go.ts
+++ test/go.ts
@@ -1,30 +1,32 @@
 import * as chai from "chai";
 import * as path from "path";
 import * as indicators from "../src/indicators/";
-import { TestDataFactory } from "../test/testData";
+import { TestDataFactory } from "./testData";
 let jsonfile = require("jsonfile");
 
 chai.should();
 
-let sourceFile: string;
 let taResultFile: string;
 let sourceData: any;
 let taResultData: any;
-let indicator: indicators.CDLDOJISTAR;
+let indicator: indicators.CDLHANGINGMAN;
 let indicatorResults: number[];
 let indicatorOnDataRasied: boolean = false;
 
-sourceFile = path.resolve("./test/sourcedata/sourcedata.json");
-taResultFile = path.resolve("./test/talib-results/cdldojistar.json");
+taResultFile = path.resolve("./test/talib-results/cdlhangingman.json");
 sourceData = TestDataFactory.getInstance().sourceData;
 taResultData = jsonfile.readFileSync(taResultFile);
 
 indicatorResults = new Array<number>(
     sourceData.close.length - taResultData.begIndex);
 
-indicator = new indicators.CDLDOJISTAR();
+indicator = new indicators.CDLHANGINGMAN();
 let idx = 0;
 sourceData.close.forEach((value: number, index: number) => {
+
+    if (taResultData.result.outInteger[idx] === -100) {
+        console.log("g");
+    }
     if (indicator.receiveData({
         "high": sourceData.high[index],
         "low": sourceData.low[index],
